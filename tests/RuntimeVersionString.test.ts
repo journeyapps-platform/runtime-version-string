bb84e65c1ed99ae14f4f3998d497255fb228ba8b
Jean-Louis Leysens <jloleysens@gmail.com>
2019-03-27 10:48:46 +0200

    Throw on invalid input (#3)
    * Throw on invalid input
    
    * v1.0.2

--- tests/RuntimeVersionString.test.ts
+++ tests/RuntimeVersionString.test.ts
@@ -6,23 +6,11 @@ describe('RuntimeVersionString', () => {
     const second = '1.2.3-dev.something-else-here+12';
     const third = '1.3.3-rc+1';
 
-    const firstBroken = '1.1.1-bet-asd.what.f+a';
-    const secondBroken = '1.1.1-beta.branch-name+222';
-    const thirdBroken = '1.2.3-stable+3333';
-    const fourthBroken = '1.2.3-rc';
-    const fifthBroken = '1.1.1-beta+a';
-
     const c1 = parse(first);
     const c2 = parse(second);
     const c5 = parse(third);
 
     expect(c1).toBeInstanceOf(RuntimeVersionString);
-
-    expect(() => parse(firstBroken)).toThrow(ErrorCodes.NOT_RECOGNISED_TRACK);
-    expect(() => parse(secondBroken)).toThrow(ErrorCodes.BRANCHES_ON_DEV_TRACK_ONLY);
-    expect(() => parse(thirdBroken)).toThrow(ErrorCodes.NO_BUILD_NR_ON_STABLE);
-    expect(() => parse(fifthBroken)).toThrow(ErrorCodes.BUILD_NR_NOT_NUMBERIC);
-    expect(() => parse(fourthBroken)).toThrow(ErrorCodes.BUILD_NR_REQUIRED_FOR_NON_STABLE_TRACK);
 
     expect(c1.major).toBe('1');
     expect(c1.minor).toBe('2');
@@ -43,5 +31,23 @@ describe('RuntimeVersionString', () => {
     expect(c5.patch).toBe('3');
     expect(c5.track).toBe('rc');
     expect(c5.buildNr).toBe('1');
+  });
+
+  it('throws as expected', () => {
+    const firstBroken = '1.1.1-bet-asd.what.f+a';
+    const secondBroken = '1.1.1-beta.branch-name+222';
+    const thirdBroken = '1.2.3-stable+3333';
+    const fourthBroken = '1.2.3-rc';
+    const fifthBroken = '1.1.1-beta+a';
+    const sixthBroken = '123';
+    const seventhBroken = 'a';
+
+    expect(() => parse(firstBroken)).toThrow(ErrorCodes.NOT_RECOGNISED_TRACK);
+    expect(() => parse(secondBroken)).toThrow(ErrorCodes.BRANCHES_ON_DEV_TRACK_ONLY);
+    expect(() => parse(thirdBroken)).toThrow(ErrorCodes.NO_BUILD_NR_ON_STABLE);
+    expect(() => parse(fourthBroken)).toThrow(ErrorCodes.BUILD_NR_REQUIRED_FOR_NON_STABLE_TRACK);
+    expect(() => parse(fifthBroken)).toThrow(ErrorCodes.BUILD_NR_NOT_NUMBERIC);
+    expect(() => parse(sixthBroken)).toThrow(ErrorCodes.INVALID_INPUT);
+    expect(() => parse(seventhBroken)).toThrow(ErrorCodes.INVALID_INPUT);
   });
 });
