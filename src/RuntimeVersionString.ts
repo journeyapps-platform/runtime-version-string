f8d9324a928b73bcf6c2cb4d1c2747b801748f1e
WesselWessels <wessel.wesselsmail@gmail.com>
2019-04-15 17:06:48 +0200

    Helper static isEmpty method (#6)
    * Add isEmpty method
    
    * Throw if null
    
    * v1.0.4

--- src/RuntimeVersionString.ts
+++ src/RuntimeVersionString.ts
@@ -186,6 +186,19 @@ export class RuntimeVersionString {
     return new RuntimeVersionString({ major: null, minor: null, patch: null, track: 'dev' });
   }
 
+  static isEmpty(runtimeVersion: RuntimeVersionString): boolean {
+    if (!runtimeVersion) {
+      throw new Error(ErrorCodes.INVALID_INPUT);
+    }
+    return (
+      runtimeVersion.major == null &&
+      runtimeVersion.minor == null &&
+      runtimeVersion.patch == null &&
+      runtimeVersion.branch == null &&
+      runtimeVersion.buildString == null
+    );
+  }
+
   static parseBuildString(buildString: string | string[]): BuildStringComponent {
     let buildObject = typeof buildString == 'string' ? buildString.split('.') : (buildString as string[]);
     if (typeof buildString == 'string') {
